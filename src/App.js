b3f409b7d2dff7c86d9ad2bc1d4cf31177fdb587
rhallgarth88 <59847530+rhallgarth88@users.noreply.github.com>
2021-07-06 14:07:01 -0400

    basic navbar component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
-      <p>Shall we create a transaction keeper?</p>
+      <Navbar />
 
     </div>
   );
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>
+                <i className="fas fa-search-dollar"/>
+                Transaction<div className="keeper">Keeper</div>
+            </h1>
+            
+        </nav>
+    )
+}
+
+export default Navbar
